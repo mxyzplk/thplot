7b8f668cdb3c09e6cc3955bfd5028512b73c2e8c
Rafael de Melo Pieslak <rafael.pieslak@ceiia.com>
2023-01-27 10:38:10 +0000

    ...

--- data.py
+++ data.py
@@ -1,16 +1,26 @@
-import panda as pd
+import pandas as pd
+import matplotlib.pyplot as plt
 
 class Data:
     def __init__(self, csvfile):
-        self.df = pd.read_csv(csvfile)
+        self.df = pd.read_csv(csvfile, skipinitialspace = True)
         self.columns = list(self.df.columns.values)
 
-
-    def plot(self, columns):
-        self.df.plot.line(subplots=True,y=columns)
-        
-    
     def set_index(self, name):
         self.df.set_index(name)
+        
+        
+    def plot_series(self, listofcolumns):
+        n = len(listofcolumns)
+
+        self.df.plot(subplots=True, nrows=10, ncols=1, y=listofcolumns, figsize=(6, 6), xlabel="Time[s]")
+        #self.df.plot(subplots=True, y=['AOA','NZ'], figsize=(6, 6), xlabel="Time[s]")
+        plt.legend(loc='best')
+        
+res=Data("flsim_th.txt")
+listofcolumns=["AOA","NZ","FZWB","FZHT"]
+res.set_index("TIME")
+res.plot_series(listofcolumns)
+
         
     
